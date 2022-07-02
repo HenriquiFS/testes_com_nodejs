3272c0af08ab70b2fc91528cd5282395bc6286fe
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-02 16:17:13 -0300

    Terceira atualização

--- stats.js
+++ stats.js
@@ -1,9 +1,13 @@
 import { freemem, totalmem } from 'node:os';
 
-//console.log( `${parseInt( freemem() / 1024 / 1024 )} MB`, `${parseInt( totalmem()/1024/1024 )} MB` )
-
 const mem = parseInt(freemem() / 1024 / 1024);
 const total = parseInt(totalmem() / 1024 / 1024);
 const percents = parseInt( (mem/total)*100 );
 
-console.log(mem,total,percents);
+const stats = {
+  free: `${mem} MB`,
+  total: `${total} MB`,
+  usage: `${percents}%`,
+}
+
+console.table(stats)
