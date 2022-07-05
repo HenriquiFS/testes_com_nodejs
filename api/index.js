e1ebf502dfc10bccf1fdabb4435aabe9662a059b
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-05 14:57:58 -0300

    Adicionando a funcionalidade de remoção de uma URL do arquivo .json

--- api/index.js
+++ api/index.js
@@ -1,9 +1,36 @@
 const http =  require('http')
 const URL = require('url')
+const fs = require('fs')
 const data = require('./urls.json')
+const path = require('path')
+
+function writeFile(cb) {
+  fs.writeFile(
+    path.join(__dirname, "urls.json"), 
+    JSON.stringify(data, null, 2),
+    err => {
+      if(err) throw err
+      cb(JSON.stringify({message: "ok"}))
+    }
+  )
+}
 
 http.createServer((req,res) => {
 
-  res.end(JSON.stringify(data))
+  const { name, url, del } = URL.parse(req.url, true).query
+
+  // http://localhost:3000
+  if(!name || !url) return res.end(JSON.stringify(data))
+  
+  // http://localhost:3000/?name=YouTube&url=http://youtube.com&del=1
+  if(del){
+    data.urls = data.urls.filter(item => String(item.url) !== String(url))
+    return writeFile( (message) => {
+      res.end(message)
+    }) 
+  }
+  
+  // http://localhost:3000/?name=Youtube&url=http://youtube.com
+  return res.end('create')
 
 }).listen(3000, () => console.log("Servidor ativado") )
